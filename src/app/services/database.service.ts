07d3bcc579474b5623bbb8d03377e4d4fe08d349
Gunnar Boström <gunnar.bos@gmail.com>
2016-11-21 09:59:13 +0100

    Added users

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -13,51 +13,65 @@ export class DatabaseService {
     // Initialize Firebase
     this.db = af.database;
     this.storeInitialData();
-    //this.storeShops();
-    //this.storeData();
+    this.storeUsers();
     // TODO remove 
     this.getShops();
   }
 
+  private users = [
+    { key: 'gunar.bos@gmail.com', name: 'Gunnar', shopOwner: 'gunar.bos@gmail.com' },
+    { key: 'lena.bost@gmail.com', name: 'Lena', shopOwner: 'gunar.bos@gmail.com' }
+  ];
+
+  private user = this.users[0];
+
+  private storeUsers() {
+    console.log('storeUsers ' + JSON.stringify(this.users));
+    const users$: FirebaseListObservable<any> = this.db.list('/users');
+    this.users.forEach(user => {
+      users$.push(user);
+    });
+  }
+
   private initialData = [
     {
-      key: "maxi", owner: "gunnar", name: "Maxi", order: 1.0, departments: [
+      owner: 'gunar.bos@gmail.com', name: 'Maxi', order: 1.0, departments: [
         {
-          key: 'frukt', owner: 'maxi', name: 'Frukt', order: 1, items: [
-            { key: "äpplen", owner: "frukt", buy: true, quantity: 1, unit: "st", name: "Äpplen", order: 1.0 },
-            { key: "päron", owner: "frukt", buy: false, quantity: 1, unit: "st", name: "Päron", order: 2.0 },
+          name: 'Frukt', owner: '', order: 1, items: [
+            { buy: true, owner: '', quantity: 1, unit: 'st', name: 'Äpplen', order: 1.0 },
+            { buy: false, owner: '', quantity: 1, unit: 'st', name: 'Päron', order: 2.0 },
           ]
         },
         {
-          key: 'mejeri', owner: 'maxi', name: 'Mejeri', order: 3.5, items: [
-            { key: "grädde", owner: "mejeri", buy: false, quantity: 8, unit: "st", name: "Grädde", order: 4.0 },
-            { key: "mjölk", owner: "mejeri", buy: false, quantity: 7, unit: "st", name: "Mjölk", order: 3.0 },
+          owner: '', name: 'Mejeri', order: 3.5, items: [
+            { buy: false, owner: 'mejeri', quantity: 8, unit: 'st', name: 'Grädde', order: 4.0 },
+            { buy: false, owner: 'mejeri', quantity: 7, unit: 'st', name: 'Mjölk', order: 3.0 },
           ]
         },
         {
-          key: 'övrigt', owner: 'maxi', name: 'Övrigt', order: 4, items: [
-            { key: "blommor", owner: "ovrigt", buy: false, quantity: 3, unit: "st", name: "Blommor", order: 6.0 }
+          owner: '', name: 'Övrigt', order: 4, items: [
+            { buy: false, owner: 'ovrigt', quantity: 3, unit: 'st', name: 'Blommor', order: 6.0 }
           ]
         },
       ]
     },
     {
-      key: "city gross", owner: "gunnar", name: "City Gross", order: 2.2, departments: [
+      owner: 'gunar.bos@gmail.com', name: 'City Gross', order: 2.2, departments: [
         {
-          key: 'diverse', owner: 'city gross', name: 'Diverse', order: 3, items: [
-            { key: "batterier", owner: "diverse", buy: false, quantity: 9, unit: "st", name: "AA Batterier", order: 5.0 },
-            { key: "lampa60", owner: "diverse", buy: false, quantity: 9, unit: "st", name: "60 W Lampa", order: 8.0 },
-            { key: "lampa40", owner: "diverse", buy: false, quantity: 9, unit: "st", name: "40 W Lampa", order: 4.0 },
+          owner: '', name: 'Diverse', order: 3, items: [
+            { buy: false, owner: '', quantity: 9, unit: 'st', name: 'AA Batterier', order: 5.0 },
+            { buy: false, owner: '', quantity: 9, unit: 'st', name: '60 W Lampa', order: 8.0 },
+            { buy: false, owner: '', quantity: 9, unit: 'st', name: '40 W Lampa', order: 4.0 },
           ]
         }
       ],
     },
-    { key: "apoteket", owner: "lena", name: "Apoteket", order: 2.2, departments: [] },
-    { key: "systemet", owner: "lena", name: "Systemet", order: 2.3, departments: [] }
+    { owner: 'gunar.bos@gmail.com', name: 'Apoteket', order: 2.2, departments: [] },
+    { owner: 'gunar.bos@gmail.com', name: 'Systemet', order: 2.3, departments: [] }
   ];
 
   private storeInitialData() {
-    console.log("Database service storeInitialData");
+    console.log('Database service storeInitialData');
     // remove old data
     const data$: FirebaseListObservable<any> =
       this.db.list('/');
@@ -67,7 +81,6 @@ export class DatabaseService {
     const departments$: FirebaseListObservable<any> = this.db.list('/departments');
     const items$: FirebaseListObservable<any> = this.db.list('/items');
     this.initialData.forEach(shop => {
-      console.log(JSON.stringify(shop));
       const departments = shop.departments;
       shop.departments = null;
       let shopKey: string = shops$.push(shop).key
@@ -85,13 +98,13 @@ export class DatabaseService {
   };
 
   getShops() {
-    console.log("DatabaseService getShops ");
+    console.log('DatabaseService getShops ');
     const shops$: FirebaseListObservable<any> = this.db.list('/shops',
       //TODO remove static query
       { query: { orderByChild: 'owner', equalTo: 'gunnar' } });
     shops$.subscribe((shops: any[]) =>
       shops.forEach(shop => {
-        console.log("shop: " + JSON.stringify(shop) + " key: " + shop.$key);
+        console.log('shop: ' + JSON.stringify(shop) + ' key: ' + shop.$key);
       }));
   };
 
