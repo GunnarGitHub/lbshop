f240aa9ee0554ee03c98134234f2441473491201
Gunnar Boström <gunnar.bos@gmail.com>
2016-11-21 08:55:46 +0100

    can load firebase with shops, deps and items

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -12,73 +12,76 @@ export class DatabaseService {
   constructor(private af: AngularFire) {
     // Initialize Firebase
     this.db = af.database;
-    this.storeDepartments();
-    this.storeShops();
+    this.storeInitialData();
+    //this.storeShops();
     //this.storeData();
     // TODO remove 
     this.getShops();
   }
 
-  private initialStores = [
-    { key: "maxi", owner: "gunnar", name: "Maxi", order: 1.0 },
-    { key: "city gross", owner: "gunnar", name: "City Gross", order: 2.2 },
-    { key: "apoteket", owner: "lena", name: "Apoteket", order: 2.2 },
-    { key: "systemet", owner: "lena", name: "Systemet", order: 2.3 }
+  private initialData = [
+    {
+      key: "maxi", owner: "gunnar", name: "Maxi", order: 1.0, departments: [
+        {
+          key: 'frukt', owner: 'maxi', name: 'Frukt', order: 1, items: [
+            { key: "äpplen", owner: "frukt", buy: true, quantity: 1, unit: "st", name: "Äpplen", order: 1.0 },
+            { key: "päron", owner: "frukt", buy: false, quantity: 1, unit: "st", name: "Päron", order: 2.0 },
+          ]
+        },
+        {
+          key: 'mejeri', owner: 'maxi', name: 'Mejeri', order: 3.5, items: [
+            { key: "grädde", owner: "mejeri", buy: false, quantity: 8, unit: "st", name: "Grädde", order: 4.0 },
+            { key: "mjölk", owner: "mejeri", buy: false, quantity: 7, unit: "st", name: "Mjölk", order: 3.0 },
+          ]
+        },
+        {
+          key: 'övrigt', owner: 'maxi', name: 'Övrigt', order: 4, items: [
+            { key: "blommor", owner: "ovrigt", buy: false, quantity: 3, unit: "st", name: "Blommor", order: 6.0 }
+          ]
+        },
+      ]
+    },
+    {
+      key: "city gross", owner: "gunnar", name: "City Gross", order: 2.2, departments: [
+        {
+          key: 'diverse', owner: 'city gross', name: 'Diverse', order: 3, items: [
+            { key: "batterier", owner: "diverse", buy: false, quantity: 9, unit: "st", name: "AA Batterier", order: 5.0 },
+            { key: "lampa60", owner: "diverse", buy: false, quantity: 9, unit: "st", name: "60 W Lampa", order: 8.0 },
+            { key: "lampa40", owner: "diverse", buy: false, quantity: 9, unit: "st", name: "40 W Lampa", order: 4.0 },
+          ]
+        }
+      ],
+    },
+    { key: "apoteket", owner: "lena", name: "Apoteket", order: 2.2, departments: [] },
+    { key: "systemet", owner: "lena", name: "Systemet", order: 2.3, departments: [] }
   ];
 
-  private storeShops() {
-    console.log("Database service storeShops");
+  private storeInitialData() {
+    console.log("Database service storeInitialData");
+    // remove old data
+    const data$: FirebaseListObservable<any> =
+      this.db.list('/');
+    data$.remove();
+
     const shops$: FirebaseListObservable<any> = this.db.list('/shops');
-    shops$.remove();
-    this.initialStores.forEach(shop => {
-      shops$.push(shop);
+    const departments$: FirebaseListObservable<any> = this.db.list('/departments');
+    const items$: FirebaseListObservable<any> = this.db.list('/items');
+    this.initialData.forEach(shop => {
+      console.log(JSON.stringify(shop));
+      const departments = shop.departments;
+      shop.departments = null;
+      let shopKey: string = shops$.push(shop).key
+      departments.forEach(department => {
+        department.owner = shopKey;
+        let keyDepartment: string = departments$.push(department).key;
+        const items = department.items;
+        department.items = null;
+        items.forEach(item => {
+          item.owner = keyDepartment;
+          items$.push(item);
+        })
+      })
     })
-  };
-
-  private initialDepartments = [
-    {
-      key: 'frukt', owner: 'maxi', name: 'Frukt', order: 1, items: [
-        { key: "äpplen", owner: "frukt", buy: true, quantity: 1, unit: "st", name: "Äpplen", order: 1.0 },
-        { key: "päron", owner: "frukt", buy: false, quantity: 1, unit: "st", name: "Päron", order: 2.0 },
-      ]
-    },
-    {
-      key: 'mejeri', owner: 'maxi', name: 'Mejeri', order: 3.5, items: [
-        { key: "grädde", owner: "mejeri", buy: false, quantity: 8, unit: "st", name: "Grädde", order: 4.0 },
-        { key: "mjölk", owner: "mejeri", buy: false, quantity: 7, unit: "st", name: "Mjölk", order: 3.0 },
-      ]
-    },
-    {
-      key: 'diverse', owner: 'city gross', name: 'Diverse', order: 3, items: [
-        { key: "batterier", owner: "diverse", buy: false, quantity: 9, unit: "st", name: "AA Batterier", order: 5.0 },
-        { key: "lampa60", owner: "diverse", buy: false, quantity: 9, unit: "st", name: "60 W Lampa", order: 8.0 },
-        { key: "lampa40", owner: "diverse", buy: false, quantity: 9, unit: "st", name: "40 W Lampa", order: 4.0 },
-      ]
-    },
-    {
-      key: 'övrigt', owner: 'maxi', name: 'Övrigt', order: 4, items: [
-        { key: "blommor", owner: "ovrigt", buy: false, quantity: 3, unit: "st", name: "Blommor", order: 6.0 }
-      ]
-    }
-  ]
-
-  private storeDepartments() {
-    console.log("Database service storeDepartments");
-    const departments$: FirebaseListObservable<any> =
-      this.db.list('/departments');
-    departments$.remove();
-    this.initialDepartments.forEach(department => {
-      const items = department.items;
-      department.items = null;
-      departments$.push(department);
-      console.log(JSON.stringify(department));
-    })
-  };
-
-
-  storeData() {
-    let key = this.db.ref('/shops').push().key;
-    this.db.ref('/shops/' + key).update({ name: 'ica', location: 'Birsta' })
   };
 
   getShops() {
